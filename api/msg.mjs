634d75866ceb2fff0a1756296c5de71a05daa612
wenxuan27 <40556700+wenxuan27@users.noreply.github.com>
2021-04-24 18:47:34 -0400

    general update

--- api/msg.mjs
+++ api/msg.mjs
@@ -1,23 +1,8 @@
 import { reddit } from './signin.mjs'
 
 
-
-let msg = reddit.getMessage('0')
-
-
-// console.log(reddit)
-// msg.fetch().then(
-//     res => console.log(res)
-// )
-
-// let inbox = reddit.getInbox().then(
-//         res => console.log(res)
-//     )
-
-// reddit.getNewModmailConversation('75hxt').then(
-//     res => console.log(res)
-// )
-// console.log(inbox)
-
-
-reddit.getUser('wenxuan27').sendMessage("he")
+reddit.composeMessage({
+    to: 'wenxuan27',
+    subject: "test test test",
+    text: 'test 1 2 1 2 123'
+})
