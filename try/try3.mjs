aa5248d6bd984a3c420db505bafac4b8ca2e643f
wenxuan27 <40556700+wenxuan27@users.noreply.github.com>
2021-05-16 18:01:25 -0400

    added a reddit ama scraper and a sender and a file reader

--- try/try3.mjs
+++ try/try3.mjs
@@ -3,23 +3,94 @@
 import { reddit } from '../api/signin.mjs'
 
 
-let sub = reddit.getSubreddit("AltStreetBets")
+async function run() {
+    let sub = reddit.getSubreddit("AltStreetBets")
 
-// console.log(sub)
+    // console.log(sub)
 
-reddit.composeMessage({
-    to: 'back_to_the_ussr',
-    subject: "test asb",
-    text: 'test asb',
-    fromSubreddit: sub
-}).then((res) => {
-    console.log(res)
-})
+    let msg =
+
+        `Dear Altist,
+
+Here is your link to claim your NFT from the [Rubic AMA](https://www.reddit.com/r/AltStreetBets/comments/naqn1p/rubic_team_is_here_ask_us_anything/) on r/AltStreetBets.
+
+All official POAPs from ASB can be seen [here](https://app.poap.xyz/scan/0xF65fB449ECa1ef8a43e7507ab7E83365DBd308c1).
+
+*May your bags be heavy*
+
+The mods of r/AltStreetBets
+
+`
+
+    let link = 'http://POAP.xyz/claim/0b5w66'
+
+    let timeout = 0
+
+    let notSent = true
+
+    while (notSent) {
+
+        // await setTimeout(() => {
+        //     notSent = false;
+
+        //     reddit.composeMessage({
+        //         to: 'wenxuan27',
+        //         subject: "1 ASB Rubic AMA NFT!",
+        //         text: msg + link,
+        //         fromSubreddit: "ethereum"
+        //     }).catch((res) => {
+        //         console.log("error: " + res)
+
+        //         notSent = true;
+        //         timeout = 1000;
+        //     })
+
+        // }, timeout)
+
+        notSent = false;
+
+        // try {
+
+        await reddit.composeMessage({
+            to: 'wenxuan27',
+            subject: "1 ASB Rubic AMA NFT!",
+            text: msg + link,
+            fromSubreddit: "ethereum"
+        }).catch((res) => {
+            console.log("error: " + res)
+
+            notSent = true;
+            timeout = 1000;
+        })
+        // } catch (error) {
+        //     console.log("error: " + res)
+
+        //     notSent = true;
+        //     timeout = 1000;
+        // }
+
+
+        if (notSent) {
+            await sleep(1000)
+
+        }
+
+
+        console.log(notSent)
+    }
 
 
 
-// reddit.composeMessage({
-//     to: 'wenxuan27',
-//     subject: "test test test",
-//     text: 'test 1 2 1 2 123'
-// })
+
+
+
+}
+
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+run()
